c88bb8f0707ce26943869c799809909a443191c2
Phil Reither <philipp.reither@gmail.com>
2017-03-08 16:22:41 +0100

    Add code from class

--- 01 - Image Tilt/Work done in class/code.js
+++ 01 - Image Tilt/Work done in class/code.js
@@ -2,12 +2,12 @@
 
 
 function tiltElement(event) {
-    let x = event.offsetX / this.offsetWidth;
-    let y = event.offsetY / this.offsetHeight;
+    let x = event.offsetX / this.offsetWidth - 0.5;
+    let y = event.offsetY / this.offsetHeight - 0.5;
     
     console.log('mouse position:', x, y);
     
-    $('.background').css('transform', 'rotateX(' + x*60 + 'deg)');
+    $('.background').css('transform', `rotateY(0) rotateX(${x * 60}deg)`);
 }
 
 $('.container').on('mousemove', tiltElement);
