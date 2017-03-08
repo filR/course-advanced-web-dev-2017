141bb724620d5bfd6021df96e7ec7eac28928ee3
Phil Reither <philipp.reither@gmail.com>
2017-03-08 16:33:38 +0100

    Add code from class

--- 01 - Image Tilt/Work done in class/code.js
+++ 01 - Image Tilt/Work done in class/code.js
@@ -1,14 +1,28 @@
 'use strict';
 
-
 function tiltElement(event) {
+    
+    // get mouse position
+    // from -0.5 to 0.5, 0 is centre
     let x = event.offsetX / this.offsetWidth - 0.5;
     let y = event.offsetY / this.offsetHeight - 0.5;
     
-    console.log('mouse position:', x, y);
-    
+    // tilt entire element
     let backgroundCSS = `rotateY(${x * 10}deg) rotateX(${y * -10}deg)`;
     $('.background').css('transform', backgroundCSS);
+    
+    // move white border
+    let borderCSS = ``;
+    $('.border').css('transform', borderCSS);
+    
+    
+    // move text
+    
+    
+    // move and rotate gradient
+    
 }
 
+
+// when mouse moves over container element, call "tiltElement" function
 $('.container').on('mousemove', tiltElement);
