07eb03e176cc5f73900b69a281bc2c2ce42d1657
Phil Reither <philipp.reither@gmail.com>
2017-03-22 15:31:13 +0100

    Add work done in class

--- 03 - Audio/Work done in Class/02 - jQuery Plugin/annoyingAudio.jQuery.js
+++ 03 - Audio/Work done in Class/02 - jQuery Plugin/annoyingAudio.jQuery.js
@@ -6,6 +6,10 @@ $.fn.annoyingAudio = function () {
     let $element = $(this);
     
     console.log('Initialising Annoying Audio');
+    
+    // create <audio> element
+    let $audioElement = $('<audio src="test.mp3" autoplay loop="true" controls="true"></audio>');
+    $('body').append($audioElement);
  
     return this;
 }
