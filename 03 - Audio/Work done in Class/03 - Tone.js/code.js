56f462b223bfd1242f8ee2bc5096f4cdfa851f9e
Phil Reither <philipp.reither@gmail.com>
2017-03-22 16:45:43 +0100

    Add work done in class

--- 03 - Audio/Work done in Class/03 - Tone.js/code.js
+++ 03 - Audio/Work done in Class/03 - Tone.js/code.js
@@ -2,3 +2,13 @@ console.log('Hello World!');
 
 let testSynth = new Tone.Synth().toMaster();
 testSynth.triggerAttackRelease("C3", "8n"); // middle c for 8th note
+
+
+document.querySelectorAll('.notes button').forEach(function (button) {
+    
+    button.onmousedown = function (event) {
+        console.log('you clicked me!!!1');
+    };
+    
+})
+
