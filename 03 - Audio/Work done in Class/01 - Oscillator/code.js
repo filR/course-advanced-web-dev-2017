96fc1578c86b3f3b77e274e24f9a7892ebb53b61
Phil Reither <philipp.reither@gmail.com>
2017-03-22 14:10:47 +0100

    Add work done in class

--- 03 - Audio/Work done in Class/01 - Oscillator/code.js
+++ 03 - Audio/Work done in Class/01 - Oscillator/code.js
@@ -4,5 +4,10 @@
 let context = new window.AudioContext();
 let oscillator = context.createOscillator();
 
+oscillator.type = 'sine'; // sine | square | triangle | sawtooth
+oscillator.frequency.value = 200;
+
 oscillator.connect(context.destination);
 oscillator.start();
+
+
