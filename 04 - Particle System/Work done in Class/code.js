be0fd201ff38f820de845feb7c8f34666bbe88d5
Phil Reither <philipp.reither@gmail.com>
2017-03-29 14:54:24 +0200

    Add work done in class

--- 04 - Particle System/Work done in Class/code.js
+++ 04 - Particle System/Work done in Class/code.js
@@ -7,15 +7,31 @@ let context = canvas.getContext('2d');
 // set canvas size
 canvas.height = window.innerHeight * 2;
 canvas.width = window.innerWidth * 2;
+let height = canvas.height;
+let width = canvas.width;
 
-// draw circle
-context.beginPath();
-context.arc(50, 50, 100, 0, Math.PI * 2);
-context.fillStyle = 'pink';
-context.fill();
+let particles = [];
+
+for (let i = 0; i < 200; i++) {
+    particles.push(new Particle());
+}
+
+//particles.forEach(function (particle) {
+//    particle.draw(context);
+//});
+
+particles.forEach((particle) => {
+    particle.draw(context);
+});
 
 
-let p = new Particle();
+
+
+
+
+
+
+
 
 
 
--- 04 - Particle System/Work done in Class/particle.js
+++ 04 - Particle System/Work done in Class/particle.js
@@ -4,10 +4,16 @@ class Particle {
     constructor() {
         console.log('Particle says: HELLO');
         
+        this.x = Math.random() * width;
+        this.y = Math.random() * height;
     }
     
-    draw() {
-        
+    draw(context) {
+        // draw circle
+        context.beginPath();
+        context.arc(this.x, this.y, 100, 0, Math.PI * 2);
+        context.fillStyle = 'pink';
+        context.fill();
     }
     
     update() {
