41d6f5793b51d138faa77d69cebfc6d1b8bb8a63
Phil Reither <philipp.reither@gmail.com>
2017-03-29 14:18:42 +0200

    Add work done in class

--- 04 - Particle System/Work done in Class/code.js
+++ 04 - Particle System/Work done in Class/code.js
@@ -4,5 +4,13 @@
 let canvas = document.querySelector('canvas');
 let context = canvas.getContext('2d');
 
+// set canvas size
+canvas.height = window.innerHeight * 2;
+canvas.width = window.innerWidth * 2;
+
 
 // draw circle
+context.beginPath();
+context.arc(50, 50, 100, 0, Math.PI * 2);
+context.fillStyle = 'pink';
+context.fill();
